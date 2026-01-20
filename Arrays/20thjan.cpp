497bdfb5200047f619c0f308f474c70fa03b86dd
ashtheconquror <ayushjadhavy15@gmail.com>
2026-01-20 20:36:17 +0530

    3314. Construct the Minimum Bitwise Array I

---
+++ Arrays/20thjan.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(): val(0), next(nullptr) {}
+    ListNode(int x): val(x), next(nullptr) {}
+    ListNode(int x, ListNode *n): val(x), next(n) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result;
+
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == 2) {
+                result.push_back(-1);
+                continue;
+            }
+        
+        bool found = false;
+        for(int j = 1; j < 32; j++) {
+            if((nums[i] & (1 << j)) > 0) {//Jth bit is set
+                continue;
+            }
+            int prev = j - 1;
+
+            //set it to 0int 
+            int x = (nums[i] ^ (1 << (j - 1)));
+            result.push_back(x);
+            found = true;
+            break;
+        }
+        
+        if(found == false) {
+            result.push_back(-1);
+        }
+        }
+        return result;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Solution sol;
+    return 0;
+}
