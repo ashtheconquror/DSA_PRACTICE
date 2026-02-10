f9de76955cb091b372ca0be358b4d5c4f7e48a59
ashtheconquror <ayushjadhavy15@gmail.com>
2026-02-10 11:18:26 +0530

    potd 10th feb

---
+++ Arrays/3719. Longest Balanced Subarray I.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(): val(0), next(nullptr) {}
+    ListNode(int x): val(x), next(nullptr) {}
+    ListNode(int x, ListNode *n): val(x), next(n) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* l, TreeNode* r): val(x), left(l), right(r) {}
+};
+
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> even;
+            unordered_set<int> odd;
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) {
+                    even.insert(nums[j]);
+                } else {
+                    odd.insert(nums[j]);
+                }
+                if (even.size() == odd.size()) {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Solution sol;
+    return 0;
+}
