49dee9192207c60f2268f6f4dd17aab9024ce1c6
ashtheconquror <ayushjadhavy15@gmail.com>
2026-02-14 14:56:31 +0530

    14th feb potd

---
+++ DP/799. Champagne Tower.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double dp[101][101];
+    double champagneTower(int poured, int query_row, int query_glass) {
+        dp[0][0] = (double) poured;
+
+        for(int i = 0; i < query_row; i++) {
+            for(int j = 0; j < query_row; j++) {
+                double excess = (dp[i][j] - 1) / 2.0;
+                if(excess > 0) {
+                    dp[i+1][j] += excess;
+                    dp[i+1][j+1] += excess;
+                }
+            }
+        }
+        return min(1.0, dp[query_row][query_glass]);
+    }
+};
